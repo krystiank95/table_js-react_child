2a5bc0bcccebf8fbfdd59716d64a27a45b07cfee
krystiank95 <133586715+krystiank95@users.noreply.github.com>
2023-07-30 14:31:55 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,9 @@
+import Tabelka1 from './tabelka';
+import React from 'react'
+
+function App()  {
+ return (Tabelka1)
+  ;
+}
+
+export default App;
---
+++ tabelka.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+class Tabelka1 extends React.Component {
+    render(){
+      return
+<div>
+<table>
+<thead>
+  <tr>
+    <th>jeden</th>
+    <th>dwa</th>
+    <th>trzy</th>
+    </tr>
+</thead>
+<tbody>
+  <tr>
+    <td>test1</td>
+    <td>test2</td>
+    <td>test3</td>
+  </tr>
+</tbody>
+</table>
+</div>
+    }
+}
+
+export default Tabelka1
